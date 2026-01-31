fcb201d48c341ee4f102a4df6acc602a05f9ef68
juan071825 <juanmateoalvarez@gmail.com>
2026-01-31 19:17:10 +0100

    test(score_counter): :test_tube: Añadir más casos test provisionales.

--- src/score_counter.py
+++ src/score_counter.py
@@ -73,14 +73,22 @@ def score_counter(frames_dict):
                         
 if __name__ == '__main__':
 
-    print(frame_dict_creator('XXXXXXXXXXXX'))
-    print(frame_dict_creator('1/2/3/4/5/6/7/8/9/1/5'))
-    print(frame_dict_creator('12121212121212121212'))
-    print(frame_dict_creator('--------------------'))
-
-    print('-'*60)
-
-    print(score_counter(frame_dict_creator('XXXXXXXXXXXX')))
-    print(score_counter(frame_dict_creator('1/2/3/4/5/6/7/8/9/1/5')))
-    print(score_counter(frame_dict_creator('12121212121212121212')))
-    print(score_counter(frame_dict_creator('--------------------')))
+    assert score_counter(frame_dict_creator('XXXXXXXXXXXX')) == 300
+    assert score_counter(frame_dict_creator('1/2/3/4/5/6/7/8/9/1/5')) == 150
+    assert score_counter(frame_dict_creator('12121212121212121212')) == 30
+    assert score_counter(frame_dict_creator('--------------------')) == 0
+    assert score_counter(frame_dict_creator('XXX-----------------')) == 60
+    assert score_counter(frame_dict_creator('----------------XXXX')) == 60
+    assert score_counter(frame_dict_creator('12345123451234512345')) == 60
+    assert score_counter(frame_dict_creator('9-9-9-9-9-9-9-9-9-9-')) == 90
+    assert score_counter(frame_dict_creator('9-3561368153258-7181')) == 82
+    assert score_counter(frame_dict_creator('9-3/613/815/-/8-7/8-')) == 121
+    assert score_counter(frame_dict_creator('X9-9-9-9-9-9-9-9-9-')) == 100
+    assert score_counter(frame_dict_creator('X9-X9-9-9-9-9-9-9-')) == 110
+    assert score_counter(frame_dict_creator('XX9-9-9-9-9-9-9-9-')) == 120
+    assert score_counter(frame_dict_creator('XXX9-9-9-9-9-9-9-')) == 141
+    assert score_counter(frame_dict_creator('9-3/613/815/-/8-7/8/8')) == 131
+    assert score_counter(frame_dict_creator('5/5/5/5/5/5/5/5/5/5/5')) == 150
+    assert score_counter(frame_dict_creator('9-9-9-9-9-9-9-9-9-XXX')) == 111
+    assert score_counter(frame_dict_creator('8/549-XX5/53639/9/X')) == 149
+    assert score_counter(frame_dict_creator('X5/X5/XX5/--5/X5/')) == 175
