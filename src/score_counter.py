c442a6c94044f77d1d98cd04b2cb4fc22283c062
juan071825 <juanmateoalvarez@gmail.com>
2026-02-01 19:19:45 +0100

    refactor(score_counter): :recycle: Mejorar la legibilidad del código variables.

--- src/score_counter.py
+++ src/score_counter.py
@@ -39,54 +39,61 @@ def score_counter(frames_dict):
     score_counter = 0
     
     for frame_key, frame_bowlings in frames_dict.items():
-          
+
+          frame_first_bowling_value = BOWLING_VALUES_DICT[frame_bowlings[FIRST_BOWLING]]
+          number_bowlings_frame = len(frame_bowlings)
+          second_bowling_value = BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]] if number_bowlings_frame  >= 2 else None
+          third_bowling_value = BOWLING_VALUES_DICT[frame_bowlings[THIRD_BOWLING]] if number_bowlings_frame  == 3 else None
+
+
           if frame_key in FRAMES_KEYS_UNTIL_NINE:
-                
                
                 next_frame = frames_dict[frame_key + 1]
-                number_bowlings_next_frame = len(frames_dict[frame_key + 1])
+                number_bowlings_next_frame = len(next_frame)
+                next_bowling_value = BOWLING_VALUES_DICT[next_frame[FIRST_BOWLING]]
+                second_next_bowling_value = BOWLING_VALUES_DICT[next_frame[SECOND_BOWLING]] if number_bowlings_next_frame >= TWO_BOWLINGS else None
+                second_next_frame_first_bowling = BOWLING_VALUES_DICT[frames_dict[frame_key + 2][FIRST_BOWLING]] if number_bowlings_next_frame == ONE_BOWLING else None
                 
+
                 if frame_bowlings[FIRST_BOWLING] == STRIKE:
                     
-                    first_frame_bowling_value = BOWLING_VALUES_DICT[frame_bowlings[FIRST_BOWLING]]
-                    score_counter += first_frame_bowling_value
+                    score_counter += frame_first_bowling_value
 
-                    if number_bowlings_next_frame == ONE_BOWLING:
-                            bowlings_next_next_frame = frames_dict[frame_key + 2]
-                            score_counter += BOWLING_VALUES_DICT[next_frame[FIRST_BOWLING]] + BOWLING_VALUES_DICT[bowlings_next_next_frame[FIRST_BOWLING]]        
+                    if number_bowlings_next_frame == ONE_BOWLING:   
+                            score_counter += next_bowling_value + second_next_frame_first_bowling
 
                     elif number_bowlings_next_frame == TWO_BOWLINGS:
 
                         if next_frame[SECOND_BOWLING] == SPARE:
-                              score_counter += BOWLING_VALUES_DICT[next_frame[SECOND_BOWLING]]
+                              score_counter += second_next_bowling_value
                         else:
-                              score_counter += BOWLING_VALUES_DICT[next_frame[FIRST_BOWLING]] + BOWLING_VALUES_DICT[next_frame[SECOND_BOWLING]]
+                              score_counter += next_bowling_value + second_next_bowling_value
 
                     elif number_bowlings_next_frame == THREE_BOWLINGS:
-                            score_counter += BOWLING_VALUES_DICT[next_frame[FIRST_BOWLING]] + BOWLING_VALUES_DICT[next_frame[SECOND_BOWLING]]
+                            score_counter += next_bowling_value + second_next_bowling_value
                 
                 elif frame_bowlings[SECOND_BOWLING] == SPARE:
-                      score_counter += BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]] + BOWLING_VALUES_DICT[next_frame[FIRST_BOWLING]]
+                      score_counter += second_bowling_value + next_bowling_value
                 
                 elif frame_bowlings[FIRST_BOWLING] in OPEN_FRAME:
-                      score_counter += BOWLING_VALUES_DICT[frame_bowlings[FIRST_BOWLING]] + BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]]
+                      score_counter += frame_first_bowling_value + second_bowling_value
 
           
           else:
                 if frame_bowlings[FIRST_BOWLING] == STRIKE:
-                      score_counter += BOWLING_VALUES_DICT[frame_bowlings[FIRST_BOWLING]]
+                      score_counter += frame_first_bowling_value
                       if frame_bowlings[THIRD_BOWLING] == SPARE:
-                            score_counter += BOWLING_VALUES_DICT[frame_bowlings[THIRD_BOWLING]]
+                            score_counter += third_bowling_value
                       else:
-                            score_counter += BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]] + BOWLING_VALUES_DICT[frame_bowlings[THIRD_BOWLING]]
+                            score_counter += second_bowling_value + third_bowling_value
 
                 
                 elif frame_bowlings[SECOND_BOWLING] == SPARE:
-                      score_counter += BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]] + BOWLING_VALUES_DICT[frame_bowlings[THIRD_BOWLING]]
+                      score_counter += second_bowling_value + third_bowling_value
 
                       
                 elif frame_bowlings[FIRST_BOWLING] in OPEN_FRAME:
-                      score_counter += BOWLING_VALUES_DICT[frame_bowlings[FIRST_BOWLING]] + BOWLING_VALUES_DICT[frame_bowlings[SECOND_BOWLING]]
+                      score_counter += frame_first_bowling_value + second_bowling_value
     
     
     
